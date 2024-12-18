a0eb1abe8e511f232af3a44e553f913693325139
Jack Dockerty <jdockerty19@gmail.com>
2024-12-18 22:19:13 +0000

    feat: scaffold basic channel usage for events

--- src/bin/lynx.rs
+++ src/bin/lynx.rs
@@ -1,6 +1,7 @@
-
+use std::path::PathBuf;
 
 use axum::{
+    extract::State,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
@@ -12,7 +13,7 @@ use serde::{Deserialize, Serialize};
 #[serde(rename_all = "lowercase")]
 enum Precision {
     Nanosecond,
-    Microsecond
+    Microsecond,
 }
 
 /// The level of persistence to run the server in, this dictates how ingested
@@ -22,9 +23,10 @@ enum Precision {
 /// - Remote means that events are ingested and parquet files are written into
 ///   an object store implementation.
 #[derive(Debug)]
+#[allow(dead_code)]
 enum Persistence {
     Local,
-    Remote // TODO
+    Remote, // TODO
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -40,16 +42,41 @@ struct Event {
     value: i64,
     /// Arbitrary key-value metadata associated with the event.
     metadata: serde_json::Value,
+}
 
+#[derive(Debug, Clone)]
+struct ServerState {
+    ingest: tokio::sync::mpsc::Sender<Event>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
+
+    let state = ServerState { ingest: tx };
+
+    tokio::spawn(async move {
+        let events_before_persist = std::env::var("LYNX_PERSIST_EVENTS").unwrap_or("2".to_string());
+        loop {
+            let mut events_recv = 0;
+            match rx.recv().await {
+                Some(e) => {
+                    events_recv += 1;
+                    println!("{e:?}");
+                    if events_recv == events_before_persist.parse::<i64>().unwrap() {
+                        let now = chrono::Utc::now().timestamp_micros();
+                        std::fs::File::create_new(format!("lynx-{now}.parquet")).unwrap();
+                    }
+                },
+                None => {}
+            }
+        }
+    });
 
     let app = Router::new()
         .route("/health", get(health))
-        .route("/api/v1/ingest", post(ingest));
-
+        .route("/api/v1/ingest", post(ingest))
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -63,7 +90,8 @@ async fn health() -> &'static str {
     "OK"
 }
 
-async fn ingest(Json(event): Json<Event>) -> impl IntoResponse {
-    println!("{event:?}");
-    (StatusCode::CREATED, "DONE")
+async fn ingest(State(state): State<ServerState>, Json(event): Json<Event>) -> impl IntoResponse {
+    state.ingest.send(event).await.unwrap();
+
+    StatusCode::CREATED
 }
