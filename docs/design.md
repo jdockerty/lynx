c37b96f172f0e034543c808060cba2d39ddff56a
Jack <56563911+jdockerty@users.noreply.github.com>
2024-12-19 20:41:35 +0000

    feat(server): add basic http ingest (#1)

--- docs/design.md
+++ docs/design.md
@@ -26,6 +26,11 @@ Simple HTTP API with JSON expected as the inbound format.
 
 Data is written to parquet files for use with the query tier.
 
+Persistence levels define how data is held:
+
+- Local persistence means that the data itself is simply written to local parquet files for the current server.
+- Remote persistence means that data is written to a remote store, e.g. S3.
+
 ### Query
 
 Built ontop of [Apache DataFusion](https://datafusion.apache.org/).
---
+++ src/bin/lynx.rs
@@ -0,0 +1,127 @@
+use std::sync::Arc;
+
+use arrow::array::{ArrayRef, RecordBatch, StringBuilder, TimestampMicrosecondBuilder, UInt64Builder};
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use parquet::arrow::ArrowWriter;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Precision {
+    Nanosecond,
+    Microsecond,
+}
+
+/// The level of persistence to run the server in, this dictates how ingested
+/// events are persisted.
+///
+/// - Local means that events are ingested and written to local parquet files.
+/// - Remote means that events are ingested and parquet files are written into
+///   an object store implementation.
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Persistence {
+    Local,
+    Remote, // TODO
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Event {
+    /// Name of the event which is being recorded.
+    name: String,
+    /// Timestamp that the event occurred.
+    timestamp: i64,
+    /// Optional precision of the provided timestamp. When this is not provided,
+    /// nanosecond precision is assumed.
+    precision: Option<Precision>,
+    /// Value associated with the event.
+    value: i64,
+    /// Arbitrary key-value metadata associated with the event.
+    metadata: serde_json::Value,
+}
+
+#[derive(Debug, Clone)]
+struct ServerState {
+    ingest: tokio::sync::mpsc::Sender<Event>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
+
+    let state = ServerState { ingest: tx };
+    // In-memory tracker for persisted files.
+    let mut persisted_files = Vec::new();
+
+    tokio::spawn(async move {
+        let events_before_persist: i64 = std::env::var("LYNX_PERSIST_EVENTS").unwrap_or("2".to_string()).parse().unwrap();
+        let mut events_buf = Vec::new();
+        let mut events_recv = 0;
+        loop {
+            match rx.recv().await {
+                Some(event) => {
+                    println!("{event:?}");
+                    events_buf.push(event);
+                    events_recv += 1;
+                    if events_recv == events_before_persist {
+                        println!("Persisting events");
+                        let now = chrono::Utc::now().timestamp_micros();
+                        let filename = format!("lynx-{now}.parquet");
+                        let file = std::fs::File::create_new(&filename).unwrap();
+
+                        let mut names = StringBuilder::new();
+                        let mut values = UInt64Builder::new();
+                        // TODO: precision hints
+                        let mut timestamps = TimestampMicrosecondBuilder::new();
+
+                        for event in &events_buf {
+                            names.append_value(&event.name);
+                            values.append_value(event.value as u64);
+                            timestamps.append_value(event.timestamp);
+                        }
+
+                        let names = Arc::new(names.finish()) as ArrayRef;
+                        let values = Arc::new(values.finish()) as ArrayRef;
+                        let timestamps = Arc::new(timestamps.finish()) as ArrayRef;
+
+                        let batch = RecordBatch::try_from_iter(vec![("timestamp", timestamps), ("name", names), ("value", values)]).unwrap();
+                        let mut writer = ArrowWriter::try_new(file, batch.schema(), None).unwrap();
+                        writer.write(&batch).unwrap();
+                        writer.close().unwrap();
+                        persisted_files.push(filename);
+                        events_recv = 0;
+                        events_buf.clear();
+                    }
+                },
+                None => {}
+            }
+        }
+    });
+
+    let app = Router::new()
+        .route("/health", get(health))
+        .route("/api/v1/ingest", post(ingest))
+        .with_state(state);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await?;
+
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+async fn health() -> &'static str {
+    "OK"
+}
+
+async fn ingest(State(state): State<ServerState>, Json(event): Json<Event>) -> impl IntoResponse {
+    state.ingest.send(event).await.unwrap();
+
+    StatusCode::CREATED
+}
