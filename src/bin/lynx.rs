03dbf3a391ecf55c34a8ed7910d78595cf3540fd
Jack Dockerty <jdockerty19@gmail.com>
2025-01-03 22:35:23 +0000

    refactor: use actor pattern for persistence

--- src/bin/lynx.rs
+++ src/bin/lynx.rs
@@ -1,8 +1,3 @@
-use std::{collections::HashMap, sync::Arc};
-
-use arrow::array::{
-    ArrayRef, RecordBatch, StringBuilder, TimestampMicrosecondBuilder, UInt64Builder,
-};
 use axum::{
     extract::State,
     http::StatusCode,
@@ -10,15 +5,7 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use parquet::arrow::ArrowWriter;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "lowercase")]
-enum Precision {
-    Nanosecond,
-    Microsecond,
-}
+use lynx::{event::Event, persist::PersistHandle};
 
 /// The level of persistence to run the server in, this dictates how ingested
 /// events are persisted.
@@ -33,91 +20,21 @@ enum Persistence {
     Remote, // TODO
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Event {
-    /// Namespace where data should be written.
-    namespace: String,
-    /// Name of the event which is being recorded.
-    name: String,
-    /// Timestamp that the event occurred.
-    timestamp: i64,
-    /// Optional precision of the provided timestamp. When this is not provided,
-    /// nanosecond precision is assumed.
-    precision: Option<Precision>,
-    /// Value associated with the event.
-    value: i64,
-    /// Arbitrary key-value metadata associated with the event.
-    metadata: serde_json::Value,
-}
-
 #[derive(Debug, Clone)]
 struct ServerState {
-    ingest: tokio::sync::mpsc::Sender<Event>,
+    ingest: PersistHandle,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
-
-    let state = ServerState { ingest: tx };
-
-    tokio::spawn(async move {
-        let events_before_persist: i64 = std::env::var("LYNX_PERSIST_EVENTS")
-            .unwrap_or("2".to_string())
-            .parse()
-            .unwrap();
-        let mut mem_events: HashMap<String, Vec<Event>> = HashMap::new();
-        loop {
-            if let Some(event) = rx.recv().await {
-                println!("{event:?}");
-                let in_mem_event = mem_events
-                    .entry(event.namespace.clone())
-                    .and_modify(|f| f.push(event.clone()))
-                    .or_default();
-
-                if in_mem_event.len() == events_before_persist as usize {
-                    println!("Persisting events for {}", event.namespace);
-                    for (namespace, events) in &mem_events {
-                        let path = format!("/tmp/lynx/{namespace}");
-
-                        if !std::fs::exists(&path).unwrap() {
-                            std::fs::create_dir_all(&path).unwrap();
-                        }
-
-                        let now = chrono::Utc::now().timestamp_micros();
-                        let filename = format!("lynx-{now}.parquet");
-                        let file = std::fs::File::create_new(format!("{path}/{filename}")).unwrap();
-                        let mut names = StringBuilder::new();
-                        let mut values = UInt64Builder::new();
-                        // TODO: precision hints
-                        let mut timestamps = TimestampMicrosecondBuilder::new();
-
-                        for event in events {
-                            names.append_value(&event.name);
-                            values.append_value(event.value as u64);
-                            timestamps.append_value(event.timestamp);
-                        }
-
-                        let names = Arc::new(names.finish()) as ArrayRef;
-                        let values = Arc::new(values.finish()) as ArrayRef;
-                        let timestamps = Arc::new(timestamps.finish()) as ArrayRef;
-
-                        let batch = RecordBatch::try_from_iter(vec![
-                            ("timestamp", timestamps),
-                            ("name", names),
-                            ("value", values),
-                        ])
-                        .unwrap();
-
-                        let mut writer = ArrowWriter::try_new(file, batch.schema(), None).unwrap();
-                        writer.write(&batch).unwrap();
-                        writer.close().unwrap();
-                    }
-                    mem_events.clear();
-                }
-            }
-        }
-    });
+    let events_before_persist: i64 = std::env::var("LYNX_PERSIST_EVENTS")
+        .unwrap_or("2".to_string())
+        .parse()
+        .unwrap();
+    let persist_handle = PersistHandle::new(events_before_persist);
+    let state = ServerState {
+        ingest: persist_handle,
+    };
 
     let app = Router::new()
         .route("/health", get(health))
@@ -134,8 +51,11 @@ async fn health() -> &'static str {
     "OK"
 }
 
-async fn ingest(State(state): State<ServerState>, Json(event): Json<Event>) -> impl IntoResponse {
-    state.ingest.send(event).await.unwrap();
+async fn ingest(
+    State(mut state): State<ServerState>,
+    Json(event): Json<Event>,
+) -> impl IntoResponse {
+    state.ingest.handle_event(event).await;
 
     StatusCode::CREATED
 }
---
+++ src/persist.rs
@@ -0,0 +1,94 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use arrow::array::{
+    ArrayRef, RecordBatch, StringBuilder, TimestampMicrosecondBuilder, UInt64Builder,
+};
+use parquet::arrow::ArrowWriter;
+use tokio::sync::mpsc::{Receiver, Sender};
+
+use crate::event::Event;
+
+#[derive(Debug, Clone)]
+pub struct PersistHandle {
+    events_queue: Sender<Event>,
+}
+
+impl PersistHandle {
+    pub fn new(max_events: i64) -> Self {
+        let (tx, rx) = tokio::sync::mpsc::channel(100);
+        let actor = PersistActor::new(max_events, rx);
+        tokio::spawn(run_persist_actor(actor));
+
+        Self { events_queue: tx }
+    }
+
+    pub async fn handle_event(&mut self, event: Event) {
+        self.events_queue.send(event).await.unwrap();
+    }
+}
+
+pub struct PersistActor {
+    max_events: i64,
+    event_receiver: Receiver<Event>,
+}
+
+impl PersistActor {
+    pub fn new(max_events: i64, event_receiver: Receiver<Event>) -> Self {
+        Self {
+            max_events,
+            event_receiver,
+        }
+    }
+}
+
+pub async fn run_persist_actor(mut actor: PersistActor) {
+    let mut mem_events: HashMap<String, Vec<Event>> = HashMap::new();
+    while let Some(event) = actor.event_receiver.recv().await {
+        let in_mem_event = mem_events
+            .entry(event.namespace.clone())
+            .and_modify(|f| f.push(event.clone()))
+            .or_insert_with(|| vec![event.clone()]);
+
+        if in_mem_event.len() == actor.max_events as usize {
+            println!("Persisting events for {}", event.namespace);
+            for (namespace, events) in &mem_events {
+                let path = format!("/tmp/lynx/{namespace}");
+
+                if !std::fs::exists(&path).unwrap() {
+                    std::fs::create_dir_all(&path).unwrap();
+                }
+
+                let now = chrono::Utc::now().timestamp_micros();
+                let filename = format!("lynx-{now}.parquet");
+                let file = std::fs::File::create_new(format!("{path}/{filename}")).unwrap();
+                let mut names = StringBuilder::new();
+                let mut values = UInt64Builder::new();
+                // TODO: precision hints
+                let mut timestamps = TimestampMicrosecondBuilder::new();
+
+                for event in events {
+                    names.append_value(&event.name);
+                    values.append_value(event.value as u64);
+                    timestamps.append_value(event.timestamp);
+                }
+
+                let names = Arc::new(names.finish()) as ArrayRef;
+                let values = Arc::new(values.finish()) as ArrayRef;
+                let timestamps = Arc::new(timestamps.finish()) as ArrayRef;
+
+                let batch = RecordBatch::try_from_iter(vec![
+                    ("timestamp", timestamps),
+                    ("name", names),
+                    ("value", values),
+                ])
+                .unwrap();
+
+                let mut writer = ArrowWriter::try_new(file, batch.schema(), None).unwrap();
+                writer.write(&batch).unwrap();
+                writer.close().unwrap();
+            }
+            mem_events.clear();
+        }
+    }
+}
